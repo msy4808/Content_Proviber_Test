fc36f30a6a9e933ab1dea1157b36f87ffe12572a
문 성 운 <mkik90911@naver.com>
2022-02-10 19:41:51 +0900

    Content Provider Create

---
+++ app/src/main/java/com/moon/content_proviber_test/ContentProvider.java
@@ -0,0 +1,58 @@
+package com.moon.content_proviber_test;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+public class ContentProvider extends ContentProvider {
+
+    SQLiteDatabase db;
+    static final String TABLE_NAME = "People";
+    // URI 변수
+    public static final String CP_AUTHORITY = "com.moon.database";
+    public static final Uri CONTENT_URI =
+            Uri.parse("content://"+ CP_AUTHORITY + "/People");
+
+
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // Implement this to handle requests to delete one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        // TODO: Implement this to handle requests for the MIME type of the data
+        // at the given URI.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        // TODO: Implement this to handle requests to insert a new row.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public boolean onCreate() {
+        // TODO: Implement this to initialize your content provider on startup.
+        return false;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        // TODO: Implement this to handle query requests from clients.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        // TODO: Implement this to handle requests to update one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
